1a144d030fd53879d60d080d8632bcfa369d53c7
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-02-28 21:03:40 +0530

    Added: UC1-CalculateLengthOfLine

--- src/com/example/linecomparison/LineComparison.java
+++ src/com/example/linecomparison/LineComparison.java
@@ -1,7 +1,21 @@
 package com.example.linecomparison;
-
+import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value of x1 : ");
+        int x1 = in.nextInt();
+        System.out.println("Enter the value of y1 : ");
+        int y1 = in.nextInt();
+        System.out.println("Enter the value of x2 : ");
+        int x2 = in.nextInt();
+        System.out.println("Enter the value of y2 : ");
+        int y2 = in.nextInt();
+
+        double dist = Math.sqrt( ( x2 - x1 ) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ));
+
+        System.out.println("Length of the line is : "+dist);
     }
 }
